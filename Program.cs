eb7da73090f288b4d166ae56871d647b5ea630c2
Abel Jesus <quincoseabel@gmail.com>
2023-05-06 18:40:05 -0400

    integrada la clase query

--- Program.cs
+++ Program.cs
@@ -4,25 +4,26 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 
+
 public class ManejoArchivos
 {
     // Constructor...
-    public ManejoArchivos(string RutaCarpeta )
+    public ManejoArchivos(string RutaCarpeta)
     {
         rutaCarpeta = RutaCarpeta;
-        ArchivosTxt = Directory.GetFiles(this.rutaCarpeta,"*.txt");
+        ArchivosTxt = Directory.GetFiles(this.rutaCarpeta, "*.txt");
         ContenidoArchivos = new string[ArchivosTxt.Length];
-        
+
     }
-    public string rutaCarpeta{get;set;}
-    public string[] ArchivosTxt{get;set;}
-    public string[] ContenidoArchivos{get;set;}
-    public Dictionary<string, string[]> PalabrasUnicas= new Dictionary<string, string[]>();
+    public string rutaCarpeta { get; set; }
+    public string[] ArchivosTxt { get; set; }
+    public string[] ContenidoArchivos { get; set; }
+    public Dictionary<string, string[]> PalabrasUnicas = new Dictionary<string, string[]>();
     public Dictionary<string, string[]> NombresvsPalabras = new Dictionary<string, string[]>();
-    public Dictionary<string, Dictionary< string,double>> Textos_Palabras_TF = new Dictionary<string, Dictionary< string,double>>(); 
-    public Dictionary<string, Dictionary< string,double>> Textos_Palabras_IDF = new Dictionary<string, Dictionary< string,double>>(); 
- public Dictionary<string, Dictionary< string,double>> DiccionarioTF_IDF = new Dictionary<string, Dictionary< string,double>>();
-    
+    public Dictionary<string, Dictionary<string, double>> Textos_Palabras_TF = new Dictionary<string, Dictionary<string, double>>();
+    public Dictionary<string, Dictionary<string, double>> Textos_Palabras_IDF = new Dictionary<string, Dictionary<string, double>>();
+    public Dictionary<string, Dictionary<string, double>> DiccionarioTF_IDF = new Dictionary<string, Dictionary<string, double>>();
+
 
     // Metodo para leer el contenido de cada array y almacenarlo en el array correspondinte
     public string[] ObtenerTextos()
@@ -38,13 +39,13 @@ public class ManejoArchivos
         return this.ContenidoArchivos;
     }
 
-   
+
 
     // Metodo para obtener palabras (Tokenizador)
     public void ObtenerPalabras()
     {
         // dividir el string en palabras 
-        char[] delimitadores = { ' ', ',', '.', ':', '¿', '?','!','*','/','"','#',')','(', };
+        char[] delimitadores = { ' ', ',', '.', ':', '¿', '?', '!', '*', '/', '"', '#', ')', '(', };
         string StringTexto = "";
         for (int i = 0; i < this.ContenidoArchivos.Length; i++)
         {
@@ -56,11 +57,36 @@ public class ManejoArchivos
 
             }
         }
-        
 
-    } 
-      
-      /// metodo contador para tf-idf
+
+    }
+
+    public double DondeEstaLaPalabra(string Palabra, Dictionary<string, string[]> Dic, string[] Texto)
+    {
+        double DocumentosDondeExiste = 0;
+        for (int i = 0; i < Texto.Length; i++)
+        {
+            string[] Documento = Dic[Texto[i]];
+
+            for (int o = 0; o < Documento.Length; o++)
+            {
+
+                if (Documento[o] == Palabra)
+                {
+                    DocumentosDondeExiste++;
+                    break;
+                }
+
+            }
+
+        }
+        return DocumentosDondeExiste;
+
+
+    }
+
+
+    ///  tf-idf
     public void Tf()
     {
         foreach (var item in this.PalabrasUnicas)
@@ -117,24 +143,25 @@ public class ManejoArchivos
             }
             this.DiccionarioTF_IDF.Add(item.Key, DiccionarioTF_IDF);
         }
-        
-    }
-   
-     
 
- public string[] Motor()
+    }
+
+
+
+    public string[] Motor()
     {
         ObtenerTextos();
         ObtenerPalabras();
         Tf();
         Idf();
         Tf_Idf();
+
         System.Console.WriteLine("Datos cargados");
         return this.ContenidoArchivos;
 
     }
-    
- 
+
+
 
 
 }
@@ -142,90 +169,108 @@ public class ManejoArchivos
 /////////////////////////query//////////////////////////////
 class Query
 {
-             ///constructor //////
-    public Query(string QueryInput )
+    ///constructor //////
+    public Query(string QueryInput)
     {
-       queryinput = QueryInput;
-       queryPalabrasUnicas = new string[0];
-       queryToken = new string[0];
+        queryinput = QueryInput;
+        queryPalabrasUnicas = new string[0];
+        queryToken = new string[0];
     }
-    
-    public string queryinput{get;set;}
-    public string[] queryPalabrasUnicas{get;set;}
-    public string[] queryToken{get;set;}
+
+    public string queryinput { get; set; }
+    public string[] queryPalabrasUnicas { get; set; }
+    public string[] queryToken { get; set; }
     public Dictionary<string, double> Query_TF = new Dictionary<string, double>();
     public Dictionary<string, double> Query_IDF = new Dictionary<string, double>();
     public Dictionary<string, double> Query_TF_IDF = new Dictionary<string, double>();
-   
-    public string[] QueryToken()
+
+
+    public void QueryToken()
     {
-        char[] delimitadores = { ' ', ',', '.', ':', '¿', '?','!','*','/','"','#',')','(', };
-       string[] queryToken = this.queryinput.Split(delimitadores);
+        char[] delimitadores = { ' ', ',', '.', ':', '¿', '?', '!', '*', '/', '"', '#', ')', '(', };
+        string[] queryToken = this.queryinput.Split(delimitadores);
+
         
-        return queryToken;
     }
-    public void QueryPalabrasMinusculas()
+    public void QueryPalabrasMinusculas(string[] queryToken)
     {
         for (int i = 0; i < this.queryToken.Length; i++)
         {
             if (queryToken != null)
             {
-             this.queryToken[i] = this.queryToken[i].ToLower();
-             this.queryPalabrasUnicas = this.queryToken.Distinct().ToArray();
+                this.queryToken[i] = this.queryToken[i].ToLower();
+                this.queryPalabrasUnicas = this.queryToken.Distinct().ToArray();
             }
         }
     }
-   //// QueryTF
-   public void Query_TF_()
-   {
-       foreach (var item in this.queryPalabrasUnicas)
-       {
-           double contador = 0;
-           foreach (var item2 in this.queryToken)
-           {
-               if (item == item2)
-               {
-                   contador++;
-               }
-           }
-           this.Query_TF.Add(item, contador / this.queryToken.Length);
-       }
-   }
-   ////QueryIDF
-    public void Query_IDF_()
+
+
+    //// QueryTF
+    public void Query_TF_()
     {
-         foreach (var item in this.queryPalabrasUnicas)
-         {
-              double contador = 0;
-              foreach (var item2 in this.queryPalabrasUnicas)
-              {
+        foreach (var item in this.queryPalabrasUnicas)
+        {
+            double contador = 0;
+            foreach (var item2 in this.queryToken)
+            {
                 if (item == item2)
                 {
-                     contador++;
-                     break;
+                    contador++;
                 }
-              }
-              this.Query_IDF.Add(item, Math.Log10(this.queryPalabrasUnicas.Length / contador));
-         }
+            }
+            this.Query_TF.Add(item, contador / this.queryToken.Length);
+        }
+    }
+    ////QueryIDF
+
+    public void Query_IDF_()
+    {
+        ManejoArchivos Objeto1 = new ManejoArchivos("E:/Mi primer proyecto/Database");
+        double TotalDeDocumentos = Objeto1.ArchivosTxt.Length;
+        for (int i = 0; i < this.queryToken.Length; i++)
+        {
+
+            this.Query_IDF.Add(this.queryToken[i], Math.Log10(TotalDeDocumentos + 1 / Objeto1.DondeEstaLaPalabra(this.queryToken[i], Objeto1.NombresvsPalabras, Objeto1.ArchivosTxt) + 1));
+
+        }
+
     }
 
+    ////QueryTF-IDF
+    public void Query_TF_IDF_()
+    {
+        foreach (var item in this.queryPalabrasUnicas)
+        {
+            this.Query_TF_IDF.Add(item, this.Query_TF[item] * this.Query_IDF[item]);
+        }
+    }
+
+    public void Motor2()
+    {
+        QueryToken();
+        QueryPalabrasMinusculas(queryToken);
+        Query_TF_();
+        Query_IDF_();
+        System.Console.WriteLine("Datos cargados");
+    }	
 
 }
 
-     
-      
+
+
 
 class Program
 {
     static void Main()
-{
-    ManejoArchivos Objeto1 = new ManejoArchivos("E:/Mi primer proyecto/Database");
-    Objeto1.Motor();
-    // System.Console.WriteLine(String.Join(", ", Objeto1.));
-    // Console.ReadLine(); // Agrega esta línea.
-} 
+    {
+        ManejoArchivos Objeto1 = new ManejoArchivos("E:/Mi primer proyecto/Database");
+        Objeto1.Motor();
+        Query Objeto2 = new Query("The Project Gutenberg");
+        Objeto2.Motor2();
+        // System.Console.WriteLine(String.Join(", ", Objeto1.));
+        // Console.ReadLine(); // Agrega esta línea.
+    }
 }
 
 
 
- 
